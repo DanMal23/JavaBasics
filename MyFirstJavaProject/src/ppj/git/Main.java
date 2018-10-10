a482869fdc0bb1f786b27031d9a1a8153cb6bab0
DanMal23 <42683024+DanMal23@users.noreply.github.com>
2018-10-10 16:42:16 +0200

    Update Main.java

--- MyFirstJavaProject/src/ppj/git/Main.java
+++ MyFirstJavaProject/src/ppj/git/Main.java
@@ -3,7 +3,7 @@ package ppj.git;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("First Java priject with Git");
+		System.out.println("First Java project with Git");
 
 	}
 
