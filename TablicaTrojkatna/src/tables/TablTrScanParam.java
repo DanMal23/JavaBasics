9477f5ff665867f46f15fb543a21f564b9a6fd45
DanMal23 <42683024+DanMal23@users.noreply.github.com>
2018-10-10 20:02:52 +0200

    Update TablTrScanParam.java

--- TablicaTrojkatna/src/tables/TablTrScanParam.java
+++ TablicaTrojkatna/src/tables/TablTrScanParam.java
@@ -7,7 +7,7 @@ public class TablTrScanParam {
 	public static void main(String[] args) {
 		int n = 0;
 		Scanner s = new Scanner(System.in);
-		System.out.print("Podaj wartoœæ parametru 'n': ");
+		System.out.print("Podaj wartoÅ›Ä‡ parametru 'n': ");
 		n = Integer.parseInt(s.next());
 		s.close();
 		for (int i = 0; i < n; i++) {
