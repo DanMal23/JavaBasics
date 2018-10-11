7b260236d86be8ac80eb165c98eaf8e83ff25663
Danuta Maliczak <danuta.m.maliczak@gmail.com>
2018-10-11 20:26:51 +0200

    Circle class

---
+++ PowierzchniaKola/src/ppj/Circle.java
@@ -0,0 +1,23 @@
+package ppj;
+
+public class Circle {
+	double radius = 0.0;
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public double area() {
+		return 3.141592 + radius + radius;
+	}
+
+	public static void main(String[] args) {
+		Circle c1 = new Circle(7);
+		Circle c2 = new Circle(8);
+		double area1 = c1.area(); // can't be private
+		double area2 = c2.area();
+		System.out.printf("Powierzchnia: %.2f", area1);
+		System.out.printf("\nPowierzchnia: %.2f", area2);
+	}
+}
+	
