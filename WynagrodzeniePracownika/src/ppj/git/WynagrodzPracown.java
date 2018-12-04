6b5951043b2cde1a6b584172eec67942038daed6
DanMal23 <42683024+DanMal23@users.noreply.github.com>
2018-12-04 13:46:31 +0100

    Update WynagrodzPracown.java

--- WynagrodzeniePracownika/src/ppj/git/WynagrodzPracown.java
+++ WynagrodzeniePracownika/src/ppj/git/WynagrodzPracown.java
@@ -6,34 +6,43 @@ public class WynagrodzPracown {
 
 	public static void main(String[] args) {
 		int przepracGodz;
-		// double pÅ‚acaMinNaGodz = 12.0;
+		double minPlacaNaGodz = 12.0;
 		double stawkaZaGodz;
-		// int normGodzTyg = 40
-		// int maxGodzTyg = 60;
+		int normGodzTyg = 40;
+		int maxGodzTyg = 60;
+		int dniPracyWMcu = 21;
+		int dniPracyWTyg = 5;
 
 		Scanner scr = new Scanner(System.in);
-		System.out.print("Podaj iloœæ przepracowanych godzin: ");
+		System.out.print("Podaj ilosc przepracowanych godzin: ");
 		przepracGodz = Integer.parseInt(scr.next());
-		System.out.print("Podaj stawka™ za godzine™: ");
+		System.out.print("Podaj stawke za godzine: ");
 		stawkaZaGodz = scr.nextInt();
 		scr.close();
 
-		if (przepracGodz > 40) {
-			double zaNadGodz = stawkaZaGodz * 1.5;
-			double wynagrTygZNadg = (przepracGodz - 40) * zaNadGodz + 40 * stawkaZaGodz;
-			System.out.printf("Wynagrodzenie tygodniowe (w tym nadgodziny): %.2f", wynagrTygZNadg);
-			double dniowkaN = wynagrTygZNadg / 5;
-			System.out.printf("\nWynagrodzenie za dzieñ pracy: %.2f", dniowkaN);
-			System.out.printf("\nWynagrodzenie miesiêczne: %.2f", dniowkaN * 21);
-			System.out.print(" PLN");
-		} else {
+		if (przepracGodz > maxGodzTyg) {
+			System.out.println("Liczba godzin pracy w tygodniu nie moze przekroczyc 60.");
+
+		} else if (stawkaZaGodz < minPlacaNaGodz) {
+			System.out.println("Stawka za godzine nie moze byc mniejsza niz 12.00 PLN.");
+		} else if (przepracGodz <= normGodzTyg) {
 			double wynagrTyg = przepracGodz * stawkaZaGodz;
 			System.out.printf("Wynagrodzenie tygodniowe: %.2f", wynagrTyg);
 			System.out.print(" PLN");
-			double dniowka = wynagrTyg / 5;
-			System.out.printf("\nWynagrodzenie za dzieñ pracy: %.2f", dniowka);
-			System.out.printf("\nŒrednie wynagrodzenie miesiêczne: %.2f", dniowka * 21);
+			double dniowka = wynagrTyg / dniPracyWTyg;
+			System.out.printf("\nWynagrodzenie za dzien pracy: %.2f", dniowka);
+			System.out.printf("\nSrednie wynagrodzenie mieseczne: %.2f", dniowka * dniPracyWMcu);
 			System.out.print(" PLN");
+		} else if (przepracGodz <= maxGodzTyg) {
+			double zaNadGodz = stawkaZaGodz * 1.5;
+			double wynagrTygZNadg = (przepracGodz - normGodzTyg) * zaNadGodz + normGodzTyg * stawkaZaGodz;
+			System.out.printf("Wynagrodzenie tygodniowe (w tym nadgodziny): %.2f", wynagrTygZNadg);
+			double dniowkaN = wynagrTygZNadg / dniPracyWTyg;
+			System.out.printf("\nWynagrodzenie za dzien pracy: %.2f", dniowkaN);
+			System.out.printf("\nWynagrodzenie miesieczne: %.2f", dniowkaN * dniPracyWMcu);
+			System.out.print(" PLN");
+
 		}
+
 	}
 }
